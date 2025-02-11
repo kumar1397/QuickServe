6496fa6cae1a8daf78c016d2868cf3a0623b2602
kumar1397 <sahuiswar007@outlook.com>
2025-02-11 18:36:48 +0530

    1

---
+++ greet.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greet(customerName string) {
+	fmt.Printf("%52s %s\n", "Namaste ", customerName)
+	fmt.Printf("%72s\n", "_/\\_ Welcome to our Restro...! _/\\_ ")
+	fmt.Println()
+}
+
+func sayTata(customerName string) {
+	fmt.Println()
+	fmt.Printf("%17s", " ")
+	fmt.Printf("_/\\_ Thank you %v for visiting Our Restro! _/\\_\n", customerName)
+	fmt.Printf("%55s\n", "We hope to see you again!")
+	fmt.Printf("%51s\n", "Have a nice day! ")
+}
